9c2ad3573754ab6becc366bdb9302b13eb33af61
Misheck <misheckmwas97@gmail.com>
2026-09-03 11:37:33 +0300

    updated projects page

--- src/data/project.js
+++ src/data/project.js
@@ -1,10 +1,13 @@
 //stores all portfolio projects
 //easy to update without touching UI code or hand coding
+// Stores all portfolio projects
+// Easy to update without touching UI code
+
 export const projects = [
   {
     title: "HealthCare Analytics Dashboard",
     description:
-      "Analysed healthcare dataset to identify disease trends and support resource alllocation decisions",
+      "Analysed healthcare data to identify disease trends and support resource allocation decisions",
     tools: ["Python", "Power BI"],
     category: "Health",
     github:
@@ -12,24 +15,37 @@ export const projects = [
     image:
       "https://i.pinimg.com/1200x/11/93/63/119363d7661db835b19f3205aa1d946f.jpg",
   },
+
   {
     title: "Superstore Dataset Analysis",
     description:
       "End-to-end retail sales analysis using SQL, Excel, and Power BI to identify profitability drivers",
-    tools: ["SQL", "Excel,", "Power BI"],
+    tools: ["SQL", "Excel", "Power BI"],
     category: "Finance",
     github: "https://github.com/Oyester254/retail-sales-analysis",
     image:
       "https://i.pinimg.com/736x/b0/c4/9e/b0c49eee87626b13dc5da09e65eb58ef.jpg",
   },
+
   {
-    title: "Education Performance Study",
+    title: "Mexico Real Estate Data Cleaning",
     description:
-      "Evaluated school performance and key factors affecting student success",
-    tools: ["Excel", "Power BI"],
-    category: "Education",
-    github: "lhttps://github.com/Oyester254/retail-sales-analysis",
+      "Cleaned, standardized, validated, and integrated real estate datasets using Python and Pandas",
+    tools: ["Python", "Pandas", "NumPy"],
+    category: "Data Analytics",
+    github: "https://github.com/Oyester254/data_science",
     image:
-      "https://i.pinimg.com/1200x/6f/b0/9f/6fb09fdb28d8a8c19e1a660b6cdf27f0.jpg",
+      "https://i.pinimg.com/736x/5b/98/24/5b982438292fc8eb441fb4e697e1e3f2.jpg",
+  },
+
+  {
+    title: "Mexico Real Estate Exploratory Analysis",
+    description:
+      "Explored property prices, size, location, and property types using statistical analysis and visualization",
+    tools: ["Python", "Pandas", "Matplotlib", "Seaborn"],
+    category: "Data Analytics",
+    github: "https://github.com/Oyester254/data_science",
+    image:
+      "https://i.pinimg.com/736x/5b/98/24/5b982438292fc8eb441fb4e697e1e3f2.jpg",
   },
 ];
